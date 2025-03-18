cf48f70eab699d6eef23224a451dbf60791c8110
Valentin <jkv21contact@gmail.com>
2025-03-18 09:41:30 -0500

    revise authorization, change navbar styling and add latest changes to sanity and the search form

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { auth, signOut, signIn } from "@/auth";
-import { BadgePlus, LogOut } from "lucide-react";
+import { BadgePlus, Github, LogOut } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Navbar = async () => {
@@ -10,6 +10,7 @@ const Navbar = async () => {
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
+        {/* Navbar Logo */}
         <Link href="/">
           <Image src="/logo.png" alt="logo" width={144} height={30} />
         </Link>
@@ -17,11 +18,13 @@ const Navbar = async () => {
         <div className="flex items-center gap-5 text-black">
           {session && session?.user ? (
             <>
+              {/* Create Link */}
               <Link href="/startup/create">
-                <span className="max-sm:hidden">Create</span>
+                <span className="max-sm:hidden font-semibold">Create</span>
                 <BadgePlus className="size-6 sm:hidden" />
               </Link>
 
+              {/* Logout Button */}
               <form
                 action={async () => {
                   "use server";
@@ -29,18 +32,24 @@ const Navbar = async () => {
                   await signOut({ redirectTo: "/" });
                 }}
               >
-                <button type="submit">
-                  <span className="max-sm:hidden">Logout</span>
-                  <LogOut className="size-6 sm:hidden text-red-500" />
+                <button
+                  type="submit"
+                  className="flex gap-2 items-center text-primary"
+                >
+                  <span className="max-sm:hidden font-semibold">Logout</span>
+                  <LogOut className="size-6 sm:hidden" />
                 </button>
               </form>
 
+              {/* User Name & Avatar */}
               <Link
                 className="flex gap-5 items-center justify-center"
                 href={`/user/${session?.id}`}
               >
-                <span className="max-sm:hidden">{session?.user?.name}</span>
-                <Avatar className="size-10 border border-gray-200">
+                <span className="max-lg:hidden font-semibold">
+                  {session?.user?.name}
+                </span>
+                <Avatar className="size-10">
                   <AvatarImage
                     src={session?.user?.image || ""}
                     alt={session?.user?.name || ""}
@@ -50,6 +59,7 @@ const Navbar = async () => {
               </Link>
             </>
           ) : (
+            // Login Button
             <form
               action={async () => {
                 "use server";
@@ -57,7 +67,13 @@ const Navbar = async () => {
                 await signIn("github");
               }}
             >
-              <button type="submit">Login</button>
+              <button
+                type="submit"
+                className="font-semibold p-3 py-2 border-[3px] border-black rounded-sm flex gap-2 items-center justify-center shadow-none hover:shadow-100 transition-all duration-150"
+              >
+                <Github className="size-5" />
+                Login
+              </button>
             </form>
           )}
         </div>
